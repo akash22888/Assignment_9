ab93f428926c53a0f5af773af073dd903cfec0a2
nitin <nitin.k.lohar@gmail.com>
2014-09-24 15:07:24 +0530

    created create_dict function

--- assgn9.py
+++ assgn9.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python
+
+# create a dictionary from the file "filename"
+def create_dict(filename):
+    f = open(filename, mode='rU')
+    lst = []
+    for line in f:
+        lst.append(line)
+    f.close()
+    i = 0
+    dictionary = {}
+    emotions = []
+    for string in lst:
+        words = string.split()
+        if len(words) < 1: continue
+        if i == 0:
+            for word in words:
+                emotions.append(word)
+        else:
+            j = 0
+            for word in words:
+                dictionary[word] = emotions[j]
+                j+=1
+        i+=1
+    return dictionary        
+
+
+def main():
+    dictionary = create_dict("mood_dict.txt")
+    print dictionary.items()
+
+if __name__ == '__main__':
+    main()
